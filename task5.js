c4b81922aff163a82d26ae619094028851c2d840
ashiouzzaman <ashiouzzaman.code@gmail.com>
2024-03-22 08:22:40 +0600

    Task 5 done

---
+++ task5.js
@@ -0,0 +1,5 @@
+// Task-5:
+// Generate a random number between 10 to 20.
+
+let randomNum = Math.floor(Math.random() * 11) + 10 // Math.floor(Math.random() * (max-min)+1) + min;
+console.log(randomNum)
